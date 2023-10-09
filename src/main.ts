ba5c72e4d0f5689702073aafabf2a4bca235cd26
GeraldGallet <gerald.gallet.gg@gmail.com>
2023-10-09 17:36:35 +0200

    feat(*): add dotenv package

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
 
-  console.log(process.env.PORT);
+  await app.listen(process.env.PORT);
+
+  console.log(`## App started on port ${process.env.PORT}`);
 }
 bootstrap();
